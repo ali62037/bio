0e6078cbcc486899b6db76e9650a5cf861490ae1
ali62037 <159637971+ali62037@users.noreply.github.com>
2024-03-23 22:53:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function toggle(){
+    var home = document.getElementById('home');
+    home.classList.toggle('active')
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active')
+}
+function toggl(){
+    var home = document.getElementById('home');
+    home.classList.toggle('active')
+    var pop = document.getElementById('pop');
+    pop.classList.toggle('active')
+}
+function togg(){
+    var home = document.getElementById('home');
+    home.classList.toggle('active')
+    var popu = document.getElementById('popu');
+    popu.classList.toggle('active')
+}
